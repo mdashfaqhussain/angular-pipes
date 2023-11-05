2dc1321568f3063dad5b8e1f3dc116743a6fbd0e
mohammed ashfaq <mdashfaqhussain510@gmail.com>
2023-11-05 16:01:25 +0530

    Angular -pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'lab-angular';
+ name:string = '';
+ date:string = '';
+ amount?:number ;
+ height?:number ;
+ miles?:number;
+
+  cars:Object = {
+      "brand": "Mercedes-Benz",
+      "model": "Citan",
+      "car body": "van",
+      "color": "red",
+      "price": "10000$",
+      "year": 2010
+    }
+
+
+ onNameChange(event:Event) {
+  this.name = (event.target as HTMLInputElement).value;
+ }
+
+ onDateChange(event:Event) {
+  this.date = (event.target as HTMLInputElement).value;
+ }
+
+ onAmountchange(event:Event) {
+  this.amount = Number((event.target as HTMLInputElement).value);
+ }
+
+ onHeightChange(event:Event) {
+  this.height = parseFloat((event.target as HTMLInputElement).value);
+ }
+
+ onMilesChange(event:Event) {
+  this.miles = parseFloat((event.target as HTMLInputElement).value);
+ }
 }
